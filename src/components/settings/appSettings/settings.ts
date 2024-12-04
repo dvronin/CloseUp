b233eef4babc2305640a2848fb7141009aacbd04
dv.ronin <dimaworonin73@gmail.com>
2024-12-04 22:01:48 +0300

    Added predefined themes

--- src/components/settings/appSettings/settings.ts
+++ src/components/settings/appSettings/settings.ts
@@ -10,10 +10,15 @@ export interface Settings {
 
 export function ChangeTheme(darkTheme: boolean) {
     const value = darkTheme ? 'invert(1)' : 'none';
-    Array.from(document.getElementsByClassName('theme')).forEach(item => {
-        const html = (item as HTMLElement);
-        html.style.filter = value;
-    });
+    const rules = Array.from(document.styleSheets[0].cssRules) as Array<CSSStyleRule>;
+
+    const index = rules.findIndex(rule => rule.selectorText == ".theme");
+    if (index == -1)
+        return;
+
+    const rule = rules[index];
+    rule.style.filter = value;
+
     Array.from(document.getElementsByTagName('canvas')).forEach(item => {
         const html = (item as HTMLElement);
         html.style.filter = value;
